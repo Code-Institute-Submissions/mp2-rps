5a87d2d8adb7b49d949114dc0165d4fb52cd2764
NatThomson <natthomsondev@gmail.com>
2022-12-07 13:51:58 +0000

    BUG 1 FIX - wrong keyword present in game.js line 9 - display: hidden - corrected to display: none

--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,10 @@
 //launches game section and hides welcome section
 document.getElementById('launch-btn').addEventListener('click', () => {
-    document.getElementById('game-section').style.display = 'block'
+    document.getElementById('game-section').style.display = 'flex'
     document.getElementById('welcome-section').style.display = 'none'
 });
 //goes back to the welcome section on reset btn click
 document.getElementById('reset-btn').addEventListener('click', () => {
+    document.getElementById('game-section').style.display = 'none'
     document.getElementById('welcome-section').style.display = 'flex'
-    document.getElementById('game-section').style.display = 'hidden'
 });
