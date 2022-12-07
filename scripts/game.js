6805b1892b898f33787fc79eacd28ce8d99b455e
NatThomson <natthomsondev@gmail.com>
2022-12-07 11:43:20 +0000

    Add js functionality to switch between welcome section and game section

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,10 @@
-document.getElementById('link-test').addEventListener('click', () => {
-    document.getElementById('link-test').style.color = 'red'
-})
+//launches game section and hides welcome section
+document.getElementById('launch-btn').addEventListener('click', () => {
+    document.getElementById('game-section').style.display = 'block'
+    document.getElementById('welcome-section').style.display = 'none'
+});
+//goes back to the welcome section on reset btn click
+document.getElementById('reset-btn').addEventListener('click', () => {
+    document.getElementById('welcome-section').style.display = 'block'
+    document.getElementById('game-section').style.display = 'hidden'
+});
