954af692d00029b3625e493a9558343f3495628d
NatThomson <natthomsondev@gmail.com>
2022-12-08 13:29:21 +0000

    Add two functions, one to incrmeent user score when user wins and one to incrment computer score when computer wins. Add these functions within the compare function in the appropriate places. Add functionality to display sier and computer scores in html

--- scripts/game.js
+++ scripts/game.js
@@ -4,48 +4,43 @@ document.getElementById('launch-btn').addEventListener('click', () => {
     document.getElementById('welcome-section').style.display = 'none'
 });
 //goes back to the welcome section on reset btn click
-//document.getElementById('reset-btn').addEventListener('click', () => {
-//    document.getElementById('game-section').style.display = 'none'
-//    document.getElementById('welcome-section').style.display = 'flex'
-//});
+document.getElementById('reset-btn').addEventListener('click', () => {
+    document.getElementById('game-section').style.display = 'none'
+    document.getElementById('welcome-section').style.display = 'flex'
+});
 
 
 /*KEY 1 = rock, 2 = paper, 3 = scissors
 1 beats 3   1 loses to 2
 2 beats 1   2 loses to 3
 3 beats 2   3 loses to 1
-
-
-
 */
+
 let playerChoice
 
+//event listers for option buttons
 choice1 = document.getElementById('rock-button').addEventListener('click', () => {
     playerChoice = parseInt('1')
-    getRandom()
+    computerChoice()
     compare()
     //console.log('player choise rock')
-})
-
+});
 choice2 = document.getElementById('paper-button').addEventListener('click', () => {
     playerChoice = parseInt('2')
-    getRandom()
+    computerChoice()
     compare()
     //console.log('player choise papier')
-})
-
+});
 choice3 = document.getElementById('scissors-button').addEventListener('click', () => {
     playerChoice = parseInt('3')
-    getRandom()
+    computerChoice()
     compare()
     //console.log('player choise scissors')
-})
+});
 
-
-
-function getRandom(){
+//gets random number between 1 and 3
+function computerChoice(){
     randomChoice = Math.floor(Math.random()*3+1)
-    //console.log(randomChoice)
 }
 
 function compare() {
@@ -54,16 +49,43 @@ function compare() {
     if (playerChoice === randomChoice) {
         console.log('you both chose the same')
     } else if (playerChoice === 1 && randomChoice === 2) {
+        incrementcomputerScore()
         console.log('computer wins, you chose rock and the computer chose paper')
     } else if (playerChoice === 1 && randomChoice === 3) {
+        incrementPlayerScore()
         console.log('user wins, you chose rock and the computer chose scissors')
     } else if (playerChoice === 2 && randomChoice === 3) {
+        incrementcomputerScore()
         console.log('computer wins, you chose paper and the computer chose scissors')
     } else if (playerChoice === 2 && randomChoice === 1) {
+        incrementPlayerScore()
         console.log('user wins, you chose paper and the computer chose rock')
     } else if (playerChoice === 3 && randomChoice === 1) {
+        incrementcomputerScore()
         console.log('computer wins, you chose scissors and the computer chose rock')
     } else if (playerChoice === 3 && randomChoice === 2) {
+        incrementPlayerScore()
         console.log('user wins, you chose scissors and the computer chose paper')
     }
+}
+
+//let addPlayerScore = document.getElementById('player-score').innerText = '0'
+//let addComputerScore = document.getElementById('computer-score').innerText = '0'
+
+//let playerScore = 0
+//let computerScore = 0
+
+
+function incrementPlayerScore() {
+let playerScore = document.getElementById('player-score')
+let playerNumber = playerScore.innerHTML
+playerNumber++
+playerScore.innerHTML = playerNumber
+}
+
+function incrementcomputerScore() {
+    letComputerScore = document.getElementById('computer-score')
+    let computerNumber = letComputerScore.innerHTML
+    computerNumber++
+    letComputerScore.innerHTML = computerNumber
 }
